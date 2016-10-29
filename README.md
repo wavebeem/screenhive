98d5865718191c46d48cd1a13c14bfd239a07cc5
Brian Mock <brian@mockbrian.com>
2016-10-29 15:11:10 -0700

    Adds static site screenhive.net

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # <img align="left" src="img/Screenhive-48px.png"> Screenhive
 
-Screenhive is an organizer for Steam PNG screenshot folders.
-
-Please read [the wiki page on setting up Steam PNG screenshots][1] before using Screenhive.
-
-<img src="screenshot.png">
-
-[1]: https://github.com/wavebeem/screenhive/wiki
+[screenhive.net](https://screenhive.net)
---
+++ docs/deploy.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+cf_distro = "E3UWPOPDXLIHRP"
+s3_dev = "s3://dev.mockbrian.com"
+s3_prod = "s3://screenhive.net"
+
+def jekyll(*args)
+  system "bundle", "exec", "jekyll", "build", *args
+end
+
+def sync(bucket)
+  system "aws", "s3", "sync",
+    "--acl", "public-read",
+    "_site/",
+    bucket
+end
+
+def invalidate(distro)
+  system "aws", "cloudfront", "create-invalidation",
+    "--distribution-id", distro,
+    "--paths", "/*"
+end
+
+if ARGV.include?("-p")
+  jekyll
+  sync s3_prod
+  invalidate cf_distro
+else
+  jekyll "--drafts"
+  s3_sync dev
+end
