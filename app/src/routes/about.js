648d1d1d50898e7e042116ca9f96914775c8da65
Brian Mock <brian@mockbrian.com>
2017-12-30 17:53:23 -0800

    github link

--- app/src/routes/about.js
+++ app/src/routes/about.js
@@ -7,6 +7,7 @@ const Link = require("../link");
 const $ = React.createElement;
 const homePage = "https://screenhive.net";
 const myPage = "https://mockbrian.com";
+const githubPage = "https://github.com/wavebeem/screenhive";
 
 function About(props) {
   const { setRoute } = props;
@@ -17,17 +18,36 @@ function About(props) {
 
   return $(
     "div",
-    {},
+    { className: "min-h-100 flex flex-column" },
     $("div", {}, $(Button, { type: "subtle", onClick: goBack }, "Back")),
-    $("p", { className: "f3 b" }, `${Package.productName} ${Package.version}`),
     $(
       "p",
-      {},
-      "Follow the ",
-      $(Link, { url: homePage }, "Steam setup instructions"),
-      " to create a screenshot folder before using this app."
+      { className: "mb0 f3 b" },
+      `${Package.productName} ${Package.version}`
     ),
-    $("p", {}, "Copyright © 2018 ", $(Link, { url: myPage }, "Brian Mock"))
+    $(
+      "div",
+      { className: "flex-auto" },
+      $(
+        "p",
+        {},
+        "Follow the ",
+        $(Link, { url: homePage }, "Steam setup instructions"),
+        " to create a screenshot folder before using this app."
+      ),
+      $(
+        "p",
+        {},
+        "GitHub: ",
+        $(Link, { url: githubPage }, "wavebeem/screenhive")
+      )
+    ),
+    $(
+      "p",
+      { className: "tc" },
+      "Copyright © 2018 ",
+      $(Link, { url: myPage }, "Brian Mock")
+    )
   );
 }
 
