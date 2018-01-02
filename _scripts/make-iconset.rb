13a29d5df83a7fd264cde405d63a90cce1654768
Brian Mock <brian@mockbrian.com>
2018-01-01 21:21:24 -0800

    new pixle art icon??

--- _scripts/make-iconset.rb
+++ _scripts/make-iconset.rb
@@ -2,12 +2,15 @@
 require "fileutils"
 include FileUtils
 
-cd "build" do
-  mkdir "icon.iconset"
+cd File.dirname($0)
+cd "../build" do
+  mkdir_p "icon.iconset"
   Dir["../img/*.png"].each do |file|
     m = file.match(%r{Screenhive-(\d+)px[.]png})
-    size = m[1].to_i
-    cp file, "icon.iconset/icon_#{size}x#{size}.png"
+    if m
+      size = m[1].to_i
+      cp file, "icon.iconset/icon_#{size}x#{size}.png"
+    end
   end
   system "iconutil", "-c", "icns", "icon.iconset"
   rm_r "icon.iconset"
