fb3be522c531e9d5767b7b8f173d70dff29b10ef
Brian Mock <brian@mockbrian.com>
2018-01-07 15:20:29 -0800

    separate icon for mac

--- _scripts/scale-icons.js
+++ _scripts/scale-icons.js
@@ -4,10 +4,12 @@ process.chdir(__dirname);
 process.chdir("../img");
 
 for (const size of [16, 32, 40, 48, 256]) {
-  spawnSync("convert", [
-    "orig.png",
-    "-scale",
-    size + "",
-    `Screenhive-${size}px.png`
-  ]);
+  for (const os of ["mac", "win"]) {
+    spawnSync("convert", [
+      `${os}-orig.png`,
+      "-scale",
+      size + "",
+      `${os}-${size}px.png`
+    ]);
+  }
 }
