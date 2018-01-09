520be7aa2d4947096ae3796bc6dd9c05af685bc1
Brian Mock <brian@mockbrian.com>
2018-01-08 21:45:31 -0800

    spinner upgrade

--- app/src/routes/working.tsx
+++ app/src/routes/working.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 export default function Working(/* props: any */) {
   return (
     <div className="center min-h-100 flex flex-column justify-center pb5">
-      <p className="tc ff3 b">Processing…</p>
-      <div className="Spinner green dib" />
+      <p className="tc f4 b">Organizing…</p>
+      <div className="Spinner center green dib w4 h4 bw1 ba br-100" />
     </div>
   );
 }
